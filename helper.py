698b5b4e7df348db46f164198218414f27221c24
aclgh <heroaclgh@gmail.com>
2024-12-25 01:28:45 +0800

    1.0

--- helper.py
+++ helper.py
@@ -1,30 +1,33 @@
 import keyboard
-
+right_key = ['j', 'k']
+right_target = 'l'
+left_key = ['d', 'f']
+left_target = 's'
 # 定义触发事件：按下两个键时模拟按下另一个键
-key_pressed = {'l': False, 's': False}
+key_pressed = {right_target: False, left_target: False}
 
 
 def on_key_event(event):
     # 监听并处理按键事件
-    if keyboard.is_pressed('j') and keyboard.is_pressed('k'):
-        if not key_pressed['l']:  # 如果目标键 'l' 没有按下
-            keyboard.press('l')
-            key_pressed['l'] = True
-            print("模拟按下 'l'")
+    if keyboard.is_pressed(right_key[0]) and keyboard.is_pressed(right_key[1]):
+        if not key_pressed[right_target]:  # 如果目标键 right_target 没有按下
+            keyboard.press(right_target)
+            key_pressed[right_target] = True
+            print(f"模拟按下 {right_target}")
     else:
-        if key_pressed['l']:  # 如果目标键 'l' 已经按下，释放它
-            keyboard.release('l')
-            key_pressed['l'] = False
+        if key_pressed[right_target]:  # 如果目标键 right_target 已经按下，释放它
+            keyboard.release(right_target)
+            key_pressed[right_target] = False
 
     if keyboard.is_pressed('d') and keyboard.is_pressed('f'):
-        if not key_pressed['s']:  # 如果目标键 's' 没有按下
-            keyboard.press('s')
-            key_pressed['s'] = True
-            print("模拟按下 's'")
+        if not key_pressed[left_target]:  # 如果目标键 left_target 没有按下
+            keyboard.press(left_target)
+            key_pressed[left_target] = True
+            print(f"模拟按下 {left_target}")
     else:
-        if key_pressed['s']:  # 如果目标键 's' 已经按下，释放它
-            keyboard.release('s')
-            key_pressed['s'] = False
+        if key_pressed[left_target]:  # 如果目标键 left_target 已经按下，释放它
+            keyboard.release(left_target)
+            key_pressed[left_target] = False
 
 
 # 设置监听
